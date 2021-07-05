7eb02fadee9a6bf87f2e60ec8b0c0e494cc263bd
sea_mountain <mm.katatumuri.48@gmail.com>
2021-07-05 16:35:36 +0900

    アプリのテーマ用のファイルを作成しました

---
+++ theme/theme.js
@@ -0,0 +1,109 @@
+
+export default theme = {
+    Night: {
+        main: {
+            mainBackgroundColor: '#888888',
+            secondBackgroundColor: '#5B5B5B',
+            titleTextColor: '#D1D1D1',
+            scrollBarColor: '#D1D1D1'
+        },
+        nav: {
+            IconColor: '',
+        },
+        menu: {
+            TitleColor: '',
+            BtnBackgroundColor: '',
+            BtnTextColor: '',
+            BtnIconColor: '',
+        },
+        menuBtn: {
+            BackgroundColor: '',
+            TextColor: '',
+            IconColor: '',
+            onPress: {
+                BackgroundColor: '',
+                TextColor: '',
+                IconColor: '',
+            }
+        },
+        menuBtnChild: {
+            BackgroundColor: '',
+            TextColor: '',
+            IconColor: '',
+            BoderColor: '',
+            onPress: {
+                BackgroundColor: '',
+                TextColor: '',
+                IconColor: '',
+                BoderColor: '',
+            }
+        },
+        PlusBtn: {
+            BackgroundColor: '',
+            IconColor: '',
+        },
+        typeSelectMenu: {
+            BackgroundColor: '',
+            TextColor: '',
+            IconColor: '',
+            onPress: {
+                BackgroundColor: '',
+                TextColor: '',
+                IconColor: '',
+            }
+        }
+    },
+    RoseQuartz: {
+        main: {
+            mainBackgroundColor: '',
+            secondBackgroundColor: '',
+            titleTextColor: '',
+            scrollBarColor: ''
+        },
+        nav: {
+            IconColor: '',
+        },
+        menu: {
+            TitleColor: '',
+            BtnBackgroundColor: '',
+            BtnTextColor: '',
+            BtnIconColor: '',
+        },
+        menuBtn: {
+            BackgroundColor: '',
+            TextColor: '',
+            IconColor: '',
+            onPress: {
+                BackgroundColor: '',
+                TextColor: '',
+                IconColor: '',
+            }
+        },
+        menuBtnChild: {
+            BackgroundColor: '',
+            TextColor: '',
+            IconColor: '',
+            BoderColor: '',
+            onPress: {
+                BackgroundColor: '',
+                TextColor: '',
+                IconColor: '',
+                BoderColor: '',
+            }
+        },
+        PlusBtn: {
+            BackgroundColor: '',
+            IconColor: '',
+        },
+        typeSelectMenu: {
+            BackgroundColor: '',
+            TextColor: '',
+            IconColor: '',
+            onPress: {
+                BackgroundColor: '',
+                TextColor: '',
+                IconColor: '',
+            }
+        }
+    },
+}
