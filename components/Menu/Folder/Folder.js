5863d1f2bb8f3c61b3335db943ac0891cbf7e617
katatumuri-maimai <mm.katatumuri.48@gmail.com>
2021-07-08 14:22:48 +0900

    フォルダーメニューを作成しました。

--- components/Menu/Folder/Folder.js
+++ components/Menu/Folder/Folder.js
@@ -1,6 +1,10 @@
-import React, { useContext, useEffect} from 'react';
+import React, { useContext, useEffect, useState} from 'react';
 import { View, Text } from 'react-native';
 import { ContextObject } from '../../../modules/context';
+import {readProjects} from '../../../modules/controlProjects';
+import MenuBtn from '../_components/MenuBtn';
+import MenuBtnChild from '../_components/MenuBtnChild';
+import MenuTitle from '../_components/MenuTitle';
 
 export default function Folder(params) {
     const {
@@ -8,5 +12,61 @@ export default function Folder(params) {
     } = useContext(ContextObject)
 
 
-    return <View></View>
+    return (
+        <View>
+            <MenuTitle>プロジェクト</MenuTitle>
+            <Project
+                project={{
+                    projectName:'test',
+                    fileList:['test','test2']
+                }}
+            />
+        </View>
+    )
+}
+
+function Project(props) {
+    const [isOnonPressMenuBtn, setOnonPressMenuBtn] = useState(false)
+    const projects = props.project
+    const projectName = projects.projectName
+    const fileList = projects.fileList
+
+    const styles={
+        nodata:{
+            color: '#FFFFFF',
+            marginTop: 30,
+        }
+    }
+
+    function onPressMenuBtn(params) {
+        { isOnonPressMenuBtn ? setOnonPressMenuBtn(false) : setOnonPressMenuBtn(true)}
+    }
+
+
+    return(
+        <View>
+            {!projectName ? <Text style={styles.nodata}>＋ボタンから新規作成</Text>:
+                <MenuBtn
+                    name={projectName}
+                    iconName={isOnonPressMenuBtn ?'folder-open':'folder'}
+                    onPress={onPressMenuBtn}
+                />
+            }
+            {isOnonPressMenuBtn?
+                (
+                    !fileList ? <Text style={styles.nodata}>＋ボタンから新規作成</Text> :
+                        (fileList.map(e=>{
+                            return(
+                                <MenuBtnChild
+                                    name={e}
+                                    iconName='text-snippet'
+                                />
+                            )
+                        }))
+                    
+                ):
+            <View/>}
+
+        </View>
+    )
 }
---
+++ modules/controlProjects.js
@@ -0,0 +1,57 @@
+import * as FileSystem from 'expo-file-system';
+import {StorageAccessFramework} from 'expo-file-system';
+
+
+export async function readProjects(os) {
+    const directoryUri = FileSystem.documentDirectory + 'SimpleMarkdown/setting/'
+    const settingFileName = 'snailSetting.json'
+    const fileUri = directoryUri + settingFileName
+    let settingData = JSON.stringify(snailSetting)
+    let FS
+
+    await FileSystem.makeDirectoryAsync(directoryUri, { intermediates: true })
+        .then(e => {
+            console.log("makeDirectoryAsync" + e);
+        }).catch(err => {
+            console.error(err);
+        })
+
+    const isFileExits = await FileSystem.getInfoAsync(fileUri)
+        .then(e => {
+            console.log("getInfoAsync >>" + e.exists);
+            return e
+        }).catch(err => {
+            console.error(err);
+        })
+
+
+    console.log(os);
+    if (os == 'iOS') {
+        FS = FileSystem
+    } else if (os == 'Android') {
+        FS = StorageAccessFramework
+    } else {
+        console.log("eeee");
+    }
+
+    if (isFileExits.exists===false) {
+    await FS.writeAsStringAsync(fileUri, settingData, { encoding: FileSystem.EncodingType.UTF8 })
+            .then(e => {
+                console.log("writeAsStringAsync >>" + e);
+                return e
+            }).catch(err => {
+                console.log(fileUri);
+                console.error("writeAsStringAsync >>" + err);
+            })
+    } else {
+        settingData = await FS.readAsStringAsync(fileUri, { encoding: FileSystem.EncodingType.UTF8 })
+            .then(e => {
+                console.log("e>>"+e);
+                return e
+            }).catch(err => {
+                console.error("readAsStringAsync >>" + err);
+            })
+    }
+    console.log("settingData>>"+settingData);
+    return JSON.parse(settingData)
+}
