9d3149b722fc78ef26b9c50a8157f285f3a8cfc6
katatumuri-maimai <mm.katatumuri.48@gmail.com>
2021-07-09 00:31:35 +0900

    保存ボタンを押したときに表示される「保存しました」コンポーネントを、1.5秒後に閉じるようにしました

--- components/TopBar/_components/SaveBtn.js
+++ components/TopBar/_components/SaveBtn.js
@@ -1,12 +1,24 @@
-import React, { useContext } from 'react';
-import { View } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { View, Text} from 'react-native';
 import { useTheme ,Icon} from 'react-native-elements';
 import { ContextObject } from '../../../modules/context';
+import { saveFile } from '../../../modules/controlProjects';
 
 export default function SaveBtn() {
     const { theme } = useTheme();
     const {
+        projectName,
+        fileName,
     } = useContext(ContextObject)
+
+    const[isSave,setIsSave]=useState(false)
+    const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+
+    useEffect(()=>{
+        openIsSave().then(e=>{
+            setIsSave(e)
+        })
+    }, [isSave])
 
     const styles={
         view:{
@@ -15,23 +27,44 @@ export default function SaveBtn() {
             position: 'absolute',
             right:20,
             top:10,
-            width: 40,
-            height:40,
+            height: 40,
+            width: isSave?'auto':40,
             backgroundColor: theme.main.secondBackgroundColor,
             borderRadius: 20,
         },
         icon:{
             fontSize: 32,
-            color: theme.nav.iconColor
+            color: theme.nav.iconColor,
+        },
+        text: {
+            color: theme.nav.iconColor,
+            margin: 10,
         }
     }
 
+    async function openIsSave() {
+        if (isSave) {
+            await sleep(1500)
+            return false
+        }
+    }
+
+    async function onPress(params) {
+        await saveFile(projectName,fileName)
+        setIsSave(true)
+    }
+
     return (
-        <View style={styles.view}>
+         isSave?
+            <View style={styles.view}><Text style={styles.text}>保存しました</Text></View >
+
+        :<View style={styles.view}>
             <Icon
-            name='save'
-            iconStyle={styles.icon}
+                name='save'
+                iconStyle={styles.icon}
+                onPress={onPress}
             />
         </View>
+        
     )
 }
