e9995614d1bd0182b94d24e52222c4e86de4eefd
katatumuri-maimai <mm.katatumuri.48@gmail.com>
2021-07-09 16:33:44 +0900

    Aquamarineテーマを追加しました。

--- modules/theme.js
+++ modules/theme.js
@@ -1,9 +1,11 @@
 export const themeList=[
     'Night',
-    'RoseQuartz'
+    'RoseQuartz',
+    'Aquamarine'
 ]
 
 export default theme = {
+    // Night
     Night: {
         main: {
             mainBackgroundColor: '#888888',
@@ -61,6 +63,7 @@ export default theme = {
             }
         }
     },
+    // RoseQuartz
     RoseQuartz: {
         main: {
             mainBackgroundColor: '#FFEEF8',
@@ -115,6 +118,292 @@ export default theme = {
                 BackgroundColor: '#BF5F82',
                 TextColor: '#F8BBD0',
                 iconColor: '#F8BBD0',
+            }
+        }
+    },
+    // Aquamarine
+    Aquamarine: {
+        main: {
+            mainBackgroundColor: '#E1F5FE',
+            secondBackgroundColor: '#B3E5FC',
+            scrollBarColor: '#B3E5FC'
+        },
+        textView: {
+            backgroundColor: '#FFFFFF',
+            textColor: '#82B3C9'
+        },
+        topBar: {
+            titleTextColor: '#FAFAFA'
+        },
+        nav: {
+            iconColor: '#FAFAFA',
+        },
+        menu: {
+            titleColor: '#E1F5FE',
+        },
+        menuBtn: {
+            BackgroundColor: '#E1F5FE',
+            TextColor: '#82B3C9',
+            iconColor: '#82B3C9',
+            onPress: {
+                BackgroundColor: '#FAFAFA',
+                TextColor: '#82B3C9',
+                iconColor: '#82B3C9',
+            }
+        },
+        menuBtnChild: {
+            BackgroundColor: 'none',
+            TextColor: '#FFFFFF',
+            iconColor: '#FFFFFF',
+            BoderColor: '#FFFFFF',
+            pickerTextColor: '#82B3C9',
+            onPress: {
+                BackgroundColor: '#FAFAFA',
+                TextColor: '#82B3C9',
+                iconColor: '#82B3C9',
+                BoderColor: 'none',
+            }
+        },
+        PlusBtn: {
+            BackgroundColor: 'none',
+            iconColor: '#FAFAFA',
+        },
+        typeSelectMenu: {
+            BackgroundColor: '#FAFAFA',
+            TextColor: '#B3E5FC',
+            iconColor: '#B3E5FC',
+            onPress: {
+                BackgroundColor: '#E1F5FE',
+                TextColor: '#FAFAFA',
+                iconColor: '#FAFAFA',
+            }
+        }
+    },
+    temp: {
+        main: {
+            mainBackgroundColor: '#',
+            secondBackgroundColor: '#',
+            scrollBarColor: '#'
+        },
+        textView: {
+            backgroundColor: '#',
+            textColor: '#'
+        },
+        topBar: {
+            titleTextColor: '#'
+        },
+        nav: {
+            iconColor: '#',
+        },
+        menu: {
+            titleColor: '#',
+        },
+        menuBtn: {
+            BackgroundColor: '#',
+            TextColor: '#',
+            iconColor: '#',
+            onPress: {
+                BackgroundColor: '#',
+                TextColor: '#',
+                iconColor: '#',
+            }
+        },
+        menuBtnChild: {
+            BackgroundColor: '#',
+            TextColor: '#',
+            iconColor: '#',
+            BoderColor: '#',
+            pickerTextColor: '#',
+            onPress: {
+                BackgroundColor: '#',
+                TextColor: '#',
+                iconColor: '#',
+                BoderColor: '#',
+            }
+        },
+        PlusBtn: {
+            BackgroundColor: '#',
+            iconColor: '#',
+        },
+        typeSelectMenu: {
+            BackgroundColor: '#',
+            TextColor: '#',
+            iconColor: '#',
+            onPress: {
+                BackgroundColor: '#',
+                TextColor: '#',
+                iconColor: '#',
+            }
+        }
+    },
+    temp2: {
+        main: {
+            mainBackgroundColor: '#',
+            secondBackgroundColor: '#',
+            scrollBarColor: '#'
+        },
+        textView: {
+            backgroundColor: '#',
+            textColor: '#'
+        },
+        topBar: {
+            titleTextColor: '#'
+        },
+        nav: {
+            iconColor: '#',
+        },
+        menu: {
+            titleColor: '#',
+        },
+        menuBtn: {
+            BackgroundColor: '#',
+            TextColor: '#',
+            iconColor: '#',
+            onPress: {
+                BackgroundColor: '#',
+                TextColor: '#',
+                iconColor: '#',
+            }
+        },
+        menuBtnChild: {
+            BackgroundColor: '#',
+            TextColor: '#',
+            iconColor: '#',
+            BoderColor: '#',
+            pickerTextColor: '#',
+            onPress: {
+                BackgroundColor: '#',
+                TextColor: '#',
+                iconColor: '#',
+                BoderColor: '#',
+            }
+        },
+        PlusBtn: {
+            BackgroundColor: '#',
+            iconColor: '#',
+        },
+        typeSelectMenu: {
+            BackgroundColor: '#',
+            TextColor: '#',
+            iconColor: '#',
+            onPress: {
+                BackgroundColor: '#',
+                TextColor: '#',
+                iconColor: '#',
+            }
+        }
+    },
+    temp3: {
+        main: {
+            mainBackgroundColor: '#',
+            secondBackgroundColor: '#',
+            scrollBarColor: '#'
+        },
+        textView: {
+            backgroundColor: '#',
+            textColor: '#'
+        },
+        topBar: {
+            titleTextColor: '#'
+        },
+        nav: {
+            iconColor: '#',
+        },
+        menu: {
+            titleColor: '#',
+        },
+        menuBtn: {
+            BackgroundColor: '#',
+            TextColor: '#',
+            iconColor: '#',
+            onPress: {
+                BackgroundColor: '#',
+                TextColor: '#',
+                iconColor: '#',
+            }
+        },
+        menuBtnChild: {
+            BackgroundColor: '#',
+            TextColor: '#',
+            iconColor: '#',
+            BoderColor: '#',
+            pickerTextColor: '#',
+            onPress: {
+                BackgroundColor: '#',
+                TextColor: '#',
+                iconColor: '#',
+                BoderColor: '#',
+            }
+        },
+        PlusBtn: {
+            BackgroundColor: '#',
+            iconColor: '#',
+        },
+        typeSelectMenu: {
+            BackgroundColor: '#',
+            TextColor: '#',
+            iconColor: '#',
+            onPress: {
+                BackgroundColor: '#',
+                TextColor: '#',
+                iconColor: '#',
+            }
+        }
+    },
+    temp4: {
+        main: {
+            mainBackgroundColor: '#',
+            secondBackgroundColor: '#',
+            scrollBarColor: '#'
+        },
+        textView: {
+            backgroundColor: '#',
+            textColor: '#'
+        },
+        topBar: {
+            titleTextColor: '#'
+        },
+        nav: {
+            iconColor: '#',
+        },
+        menu: {
+            titleColor: '#',
+        },
+        menuBtn: {
+            BackgroundColor: '#',
+            TextColor: '#',
+            iconColor: '#',
+            onPress: {
+                BackgroundColor: '#',
+                TextColor: '#',
+                iconColor: '#',
+            }
+        },
+        menuBtnChild: {
+            BackgroundColor: '#',
+            TextColor: '#',
+            iconColor: '#',
+            BoderColor: '#',
+            pickerTextColor: '#',
+            onPress: {
+                BackgroundColor: '#',
+                TextColor: '#',
+                iconColor: '#',
+                BoderColor: '#',
+            }
+        },
+        PlusBtn: {
+            BackgroundColor: '#',
+            iconColor: '#',
+        },
+        typeSelectMenu: {
+            BackgroundColor: '#',
+            TextColor: '#',
+            iconColor: '#',
+            onPress: {
+                BackgroundColor: '#',
+                TextColor: '#',
+                iconColor: '#',
             }
         }
     },
